d445b30061f38ec4beb811b0982d29448f6c524a
Tyler Benton <tylerb@marketamerica.com>
2016-09-16 11:09:53 -0400

    fixed a few errors that shouldn't error

---
+++ stylelint/namespaces.js
@@ -0,0 +1,7 @@
+/// @todo {2}
+/// Add name space rules for all patters
+///  - classes
+///  - placeholders
+///  - functions
+///  - mixins
+///  - variables
--- stylelint/scss.js
+++ stylelint/scss.js
@@ -25,15 +25,15 @@ module.exports = {
       }
     ],
     'scss/at-extend-no-missing-placeholder': true,
-    'scss/at-function-pattern': '^[a-z]{2,}[a-z-]*$',
+    'scss/at-function-pattern': '^[a-z]+(?:[a-z-]{3,})?$',
+    'scss/at-mixin-pattern': '^[a-z]+(?:[a-z-]{3,})?$',
+    'scss/dollar-variable-pattern': '^[a-z]+(?:[a-z_-]{3,})?(?:h[1-6])?$',
+    'scss/percent-placeholder-pattern': '^[a-z](?:[a-z_-]{3,})?$',
     'scss/at-import-no-partial-leading-underscore': true,
     'scss/at-import-partial-extension-blacklist': /.*/,
     'scss/at-mixin-argumentless-call-parentheses': 'always',
-    'scss/at-mixin-pattern': '^[a-z]{2,}[a-z-]*$',
     'scss/dollar-variable-colon-space-after': 'always-single-line',
     'scss/dollar-variable-colon-space-before': 'never',
-    'scss/dollar-variable-pattern': '^[a-z]{2,}[a-z-]*$',
-    'scss/percent-placeholder-pattern': '^[a-z]{2,}[a-z-]*$',
     'scss/double-slash-comment-whitespace-inside': 'always',
     // note{!} open up issue to fix the following rules on stylelint-scss
     // 'scss/declaration-nested-properties': 'never',
@@ -42,7 +42,6 @@ module.exports = {
     'scss/operator-no-newline-after': true,
     'scss/operator-no-newline-before': true,
     'scss/operator-no-unspaced': true,
-    'scss/partial-no-import': true,
     'scss/selector-no-redundant-nesting-selector': true,
   }
 };
