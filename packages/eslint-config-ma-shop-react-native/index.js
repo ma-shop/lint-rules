04ac9b21924c1354c40996333fdf4285237bf3a1
Tyler Benton <tylerb@marketamerica.com>
2018-03-05 12:21:53 -0500

    added react-native setup

--- packages/eslint-config-ma-shop-react-native/index.js
+++ packages/eslint-config-ma-shop-react-native/index.js
@@ -1,3 +1,16 @@
 module.exports = {
+  env: {
+    jest: true,
+    'react-native/react-native': true,
+  },
+  globals: {
+    __DEV__: true,
+  },
   extends: [ 'eslint-config-ma-shop-react' ],
+  plugins: [ 'react-native' ],
+  rules: {
+    // `.jsx` extension cannot be used with React Native
+    // https://github.com/airbnb/javascript/issues/982
+    'react/jsx-filename-extension': [ 'error', { extensions: [ '.js', '.jsx' ] } ],
+  },
 }
